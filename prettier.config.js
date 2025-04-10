6cfd3a5cb3386662c97b641b7ec7e4d46f3fb1d0
Tamara Cook <info@infolektuell.de>
2025-04-10 21:03:51 +0200

    Add custom example

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Options} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  singleQuote: true,
+  semi: false,
+  astroAllowShorthand: true,
+}
